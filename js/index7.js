74e699621b5f43763a9bc9cf23fb421374ad3440
illyabezpaliy13 <110487623+illyabezpaliy13@users.noreply.github.com>
2023-04-29 16:18:54 +0300

    fix4

--- js/index7.js
+++ js/index7.js
@@ -2,6 +2,6 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let sum = 0;
 for (let i = 0; i < numbers.length; i += 2) {
     console.log(`Парні числа = ${i}`)
-    sum += i;
+    sum += 1;
     console.log(sum)
 }
